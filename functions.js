f3332bc5b8c7eabf9020e07cdeae88200bff514f
Ikhwan <muhammad.nasution@amartha.com>
2022-08-20 01:03:42 +0700

    Relearn JS in order to onboarding job

---
+++ functions.js
@@ -0,0 +1,27 @@
+function sayHello(){
+    console.log("Hello");
+}
+
+var sayBye = function(){
+    console.log("Bye");
+}
+
+function sing(song){
+    console.log(song);
+}
+
+sayHello();
+sayBye();
+
+sing("Laa de daa");
+sing("Hellooooo")
+
+
+var a = function multiply(a, b){
+    if(a > 10 || b > 10){
+        return "That's too hard";
+    }
+    return a * b;
+}
+
+console.log(a(5,10));
